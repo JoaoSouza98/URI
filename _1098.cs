3a1887cbbb5ea5e76b7e9f71d443b49ea7235f5d
Joao Souza <joao_suza@outlook.com>
2019-02-09 11:39:51 -0200

    1098

---
+++ _1098.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1098
+{
+    class _1098
+    {
+        static void Main(string[] args)
+        {
+            double i = 0.0;
+            double j = 0.0;
+
+            while (i <= 2)
+            {
+                for (int aux = 1; aux <= 3; aux++)
+                {
+                    Console.WriteLine($"I={i} J={(double)aux + j}");
+                }
+                i += 0.2;
+                j += 0.2;
+            }
+        }
+    }
+}
