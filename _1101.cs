ee18c04c6efe7c01416c7ca26b9a8bae83899945
Joao Souza <joao_suza@outlook.com>
2019-02-09 12:47:51 -0200

    1101

---
+++ _1101.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _1101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1, m = 1, sum;
+
+
+            while (n > 0 && m > 0)
+            {
+                string[] _in = Console.ReadLine().Split(' ');
+                n = int.Parse(_in[0]);
+                m = int.Parse(_in[1]);
+
+                if (m > 0 && n > 0)
+                {
+                    if (n > m)
+                    {
+                        sum = 0;
+                        for (int i = m; i <= n; i++)
+                        {
+                            Console.Write($"{i} ");
+                            sum += i;
+                        }
+                        Console.WriteLine($"Sum={sum}");
+                    }
+                    else if (n < m)
+                    {
+                        sum = 0;
+                        for (int i = n; i <= m; i++)
+                        {
+                            Console.Write($"{i} ");
+                            sum += i;
+                        }
+                        Console.WriteLine($"Sum={sum}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{n} Sum={m}");
+                    }
+                }
+            }
+
+        }
+    }
+}
